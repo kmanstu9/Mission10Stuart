211fbac219b1809af21762a9fc3d0af029b8afc7
kmanstu9 <kmanstu9@byu.edu>
2025-03-14 19:11:14 -0600

    Updated .gitignore and removed unnecessary files

---
+++ backend/Mission10Stuart/Mission10Stuart/Controllers/BowlingLeagueController.cs
@@ -0,0 +1,26 @@
+﻿using Mission10Stuart.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mission10Stuart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BowlingLeagueController : ControllerBase
+    {
+        private BowlingLeagueContext _context;
+        
+        public BowlersLeagueController(BowlingLeagueContext temp)
+        {
+            _context = temp;
+        }
+
+        [HttpGet(Name = "GetTableName")]
+        public IEnumerable<Bowlers> Get()
+        {
+            var BowlersList = _context.Bowlers.ToList();
+
+            return (BowlersList);
+        }
+    }
+}
